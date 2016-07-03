ac1fe9acf7d58640d723d7db29dafbae4a83487c
ExamplePatrick <broadcast.programmer@gmail.com>
2016-07-04 01:03:09 +0900

    max and min finish
    gogo

--- ProblemEasy/ProblemEasy/MaxAndMin02.c
+++ ProblemEasy/ProblemEasy/MaxAndMin02.c
@@ -7,7 +7,7 @@
 
 #include <stdio.h>
 
-#define ARR_SIZE
+#define ARR_SIZE 5
 
 int max(int arr[], int arr_len);
 
@@ -17,12 +17,24 @@ int main() {
 
 	printf("input five integer : ");
 	
-	for (i = 0;i < )
+	for (i = 0; i < ARR_SIZE; i++) {
+		scanf("%d", &arr[i]);
+	}
 
-
-	printf("max : %d\n", max(a, b, c));
+	printf("max : %d\n", arr[max(arr, ARR_SIZE)]);
 
 	return 0;
 }
 
-int max(int arr[], int arr_len);
+int max(int arr[], int arr_len) {
+
+	int max, i;
+
+	max = 0;
+	for (i = 1; i < arr_len; i++) {
+		if (arr[i] > arr[max]) max = i;
+	}
+
+	return max;
+}
+
